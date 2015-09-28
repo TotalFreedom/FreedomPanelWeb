1723241ceea01abf8e44e3af38dfe7814bbec05f
Charles <charlesg.github@gmail.com>
2015-09-28 16:00:29 +0100

    Test environment config change.

--- src/global/config.php
+++ src/global/config.php
@@ -4,7 +4,7 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 */
-$includePath = '/Users/thecjgcjg/Google Drive/Documents/Development/FreedomPanelWeb/src/';
+$includePath = '/Users/thecjgcjg/Library/Mobile Documents/com~apple~CloudDocs/Documents/Development/FreedomPanelWeb/src/';
 
 // MySQL Details
 define('SQL_SERVER', '127.0.0.1');
